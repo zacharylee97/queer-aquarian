cba5b5f7ea1abb9247e782e58ecb146587f9b906
Zach Lee <zacharylhlee@gmail.com>
2023-11-22 14:38:55 -0700

    Added Luna to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,53 +4,63 @@ import { useTheme } from "@mui/material/styles";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import { MenuItems } from "../interfaces/menu-items";
 import MenuIcon from "@mui/icons-material/Menu";
 import MobileDrawer from "./mobile-drawer";
-import Dropdown from "./dropdown";
 
 const companyName = "Queer Aquarian";
 
 export default function Header() {
   const theme = useTheme();
+  const logoStyle = {
+    borderRadius: '50%',
+  }
 
   const [drawerState, setDrawerState] = useState(false);
 
   return (
     <Box>
       <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
-        <Toolbar disableGutters>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ m: 2 }}
-              onClick={() => setDrawerState(!drawerState)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <MobileDrawer
-              open={drawerState}
-              setDrawerState={setDrawerState}
-            ></MobileDrawer>
-          </Box>
-          <Image
-            className="m-5"
-            src="/next.svg"
-            alt="next"
-            width={80}
-            height={80}
-          />
-          <Box className="flex flex-col mx-3">
-            <Link href="/">
-              <Typography variant="h5" textAlign="center">
-                {companyName}
-              </Typography>
-            </Link>
+        <Toolbar disableGutters sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ m: 2 }}
+                onClick={() => setDrawerState(!drawerState)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <MobileDrawer
+                open={drawerState}
+                setDrawerState={setDrawerState}
+              ></MobileDrawer>
+            </Box>
+            <Image
+              className="m-5"
+              src="/luna.jpeg"
+              alt="Luna"
+              width={80}
+              height={80}
+              style={logoStyle}
+            />
+            <Box className="flex flex-col mx-3">
+              <Link href="/">
+                <Typography variant="h4" textAlign="center">
+                  {companyName}
+                </Typography>
+              </Link>
+            </Box>
           </Box>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Link
+              href="/contact-us"
+              className="flex items-center m-10 uppercase text-xl"
+            >
+              Contact Us
+            </Link>
           </Box>
         </Toolbar>
       </AppBar>
