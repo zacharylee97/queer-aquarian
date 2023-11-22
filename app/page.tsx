868305ad4097c11492d392e86f59971af5d95dc1
Zach Lee <zacharylhlee@gmail.com>
2023-11-22 13:36:02 -0700

    Added content to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/header";
 import Image from "next/image";
+import Link from "next/link";
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
@@ -7,11 +8,16 @@ export default function Home() {
     <main className="min-h-screen">
       <Header></Header>
       <Box className="flex flex-col mt-28">
-        <Typography className="m-10" variant="h3">
-          About Us
+        <Typography className="m-10" variant="h1">
+          In the meantime...
         </Typography>
         <Typography className="mx-10 mb-3" variant="h6">
-          Lorem ipsum
+          Instagram:
+          <a href="https://www.instagram.com/queer_aquarian"> https://www.instagram.com/queer_aquarian
+          </a>
+        </Typography>
+        <Typography className="mx-10 mb-3" variant="h6">
+          CLOBS Preview:
         </Typography>
       </Box>
     </main>
