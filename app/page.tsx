65b197ddf4f97f206ebbe59c5b74f8fb2d3a2e31
Zach Lee <zacharylhlee@gmail.com>
2023-11-22 13:55:15 -0700

    Added Coming Soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "./components/header";
 import Image from "next/image";
-import Link from "next/link";
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
@@ -8,6 +7,15 @@ export default function Home() {
     <main className="min-h-screen">
       <Header></Header>
       <Box className="flex flex-col mt-28">
+      <Box>
+        <Image
+              className="m-5"
+              src="/coming-soon.png"
+              alt="Coming Soon"
+              width={1000}
+              height={1000}
+            />
+      </Box>
         <Typography className="m-10" variant="h1">
           In the meantime...
         </Typography>
@@ -15,9 +23,6 @@ export default function Home() {
           Instagram:
           <a href="https://www.instagram.com/queer_aquarian"> https://www.instagram.com/queer_aquarian
           </a>
-        </Typography>
-        <Typography className="mx-10 mb-3" variant="h6">
-          CLOBS Preview:
         </Typography>
       </Box>
     </main>
