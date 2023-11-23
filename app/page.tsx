50f03259f34c81f8362da1a1a12c44178e41fbf5
Zachary Lee <zacharylhlee@gmail.com>
2023-11-22 20:57:41 -0700

    Added CLOBS

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,33 @@ export default function Home() {
     <main className="min-h-screen">
       <Header></Header>
       <Box className="flex flex-col mt-28">
-      <Box>
-        <Image
-              className="m-5"
-              src="/coming-soon.png"
-              alt="Coming Soon"
-              width={1000}
-              height={1000}
-            />
-      </Box>
+        <Box>
+          <Image
+            className="m-5"
+            src="/coming-soon.png"
+            alt="Coming Soon"
+            width={1000}
+            height={1000}
+          />
+        </Box>
         <Typography className="m-10" variant="h1">
           In the meantime...
         </Typography>
         <Typography className="mx-10 mb-3" variant="h6">
-          Instagram:
-          <a href="https://www.instagram.com/queer_aquarian"> https://www.instagram.com/queer_aquarian
+          Instagram:{" "}
+          <a href="https://www.instagram.com/queer_aquarian">
+            https://www.instagram.com/queer_aquarian
           </a>
         </Typography>
+        <Box>
+          <Image
+            className="m-5"
+            src="/CLOBS.png"
+            alt="CLOBS"
+            width={500}
+            height={500}
+          />
+        </Box>
       </Box>
     </main>
   );
