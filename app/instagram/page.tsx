2790b10eb34b916f29e3c59caa4dc10fe5bf8d1a
Zach Lee <zacharylhlee@gmail.com>
2023-11-27 09:40:41 -0700

    Fixed title styling

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -6,17 +6,19 @@ export default function Home() {
   return (
     <main className="min-h-screen w-screen">
       <Header></Header>
-      <Box className="flex flex-col mt-28 mx-10">
-        <Typography variant="h1">
-          Instagram
-        </Typography>
-        <Typography variant="h5">
-          <a href="https://www.instagram.com/queer_aquarian">
-            <u>
-              https://www.instagram.com/queer_aquarian
-            </u>
-          </a>
-        </Typography>
+      <Box className="flex flex-col mt-28">
+        <Box className="m-10 mb-3">
+          <Typography variant="h3">
+            Instagram
+          </Typography>
+          <Typography className="mt-3" variant="h5">
+            <a href="https://www.instagram.com/queer_aquarian">
+              <u>
+                https://www.instagram.com/queer_aquarian
+              </u>
+            </a>
+          </Typography>
+        </Box>
         <InstagramFeed></InstagramFeed>
       </Box>
     </main>
