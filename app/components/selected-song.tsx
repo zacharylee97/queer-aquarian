99989fb366231015d890656a03d13e28229ee642
Zach Lee <zacharylhlee@gmail.com>
2023-11-27 11:02:06 -0700

    Updated SelectedSong to useState

--- app/components/selected-song.tsx
+++ app/components/selected-song.tsx
@@ -1,11 +1,24 @@
+'use client'
+import { useEffect, useState } from 'react';
 import { Playlist } from '../constants/playlist';
+import { Song } from '../interfaces/song';
+import { Box } from '@mui/material';
 
 export default function SelectedSong() {
-  const selectedSong = Playlist[Math.floor(Math.random() * Playlist.length)];
+  const [selectedSong, setSelectedSong] = useState<Song | null>(null);
+
+  useEffect(() => {
+    setSelectedSong(Playlist[Math.floor(Math.random() * Playlist.length)]);
+  });
 
   return (
-    <i>
-      {selectedSong.title} by {selectedSong.artist}
-    </i>
+    <Box className="mx-3">
+      {selectedSong !== null ?
+        <i>
+          {selectedSong.title} by {selectedSong.artist}
+        </i>
+        : <div></div>
+      }
+    </Box>
   )
 }
