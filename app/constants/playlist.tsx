c314aa6a420bce1ff3399cbf0462066b610fc4e5
Zachary Lee <zacharylhlee@gmail.com>
2023-11-26 20:01:31 -0700

    Created Playlist constant

---
+++ app/constants/playlist.tsx
@@ -0,0 +1,174 @@
+import { Song } from "../interfaces/song";
+
+export const Playlist: Song[] = [
+  {
+    title: "One More Last Time",
+    artist: "Henry Young feat. Ashley Alisha",
+    album: "Single",
+  },
+  {
+    title: "Drama",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+  },
+  {
+    title: "Air Force One",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "Je Ne Sais Quoi",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "Lucid",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "My Secret Playlist",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "MANIAC",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Untie",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Chill Kill",
+    artist: "Red Velvet",
+    album: "Chill Kill - The 3rd Album",
+  },
+  {
+    title: "Will I Ever See You Again",
+    artist: "Red Velvet",
+    album: "Chill Kill - The 3rd Album",
+  },
+  {
+    title: "Psycho",
+    artist: "Red Velvet",
+    album: "'The ReVe Festival' Finale",
+  },
+  {
+    title: "In My Dreams",
+    artist: "Red Velvet",
+    album: "'The ReVe Festival 2022 - Feel My Rhythm'",
+  },
+  {
+    title: "Off The Record",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Baddie",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Either Way",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Holy Moly",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "OTT",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Payback",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Sensitive",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Colouring",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Strawberry soda",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Day by day",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "GODS",
+    artist: "NewJeans",
+    album: "Single",
+  },
+  {
+    title: "Beautiful Restriction",
+    artist: "NewJeans",
+    album: "Single",
+  },
+  {
+    title: "ASAP",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "Cool With You",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "OMG",
+    artist: "NewJeans",
+    album: "NewJeans 'OMG'",
+  },
+  {
+    title: "Ditto",
+    artist: "NewJeans",
+    album: "NewJeans 'OMG'",
+  },
+  {
+    title: "Don't Know What To Do",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Feel Special",
+    artist: "TWICE",
+    album: "Feel Special",
+  },
+  {
+    title: "Basics",
+    artist: "TWICE",
+    album: "BETWEEN 1&2",
+  },
+  {
+    title: "DM",
+    artist: "fromis_9",
+    album: "Midnight Guest",
+  },
+  {
+    title: "Attitude",
+    artist: "fromis_9",
+    album: "Unlock My World",
+  },
+  {
+    title: "#menow",
+    artist: "fromis_9",
+    album: "Unlock My World",
+  },
+];
