eff1cdb0bc34472ae38e20a70efef1ed7da34229
Zach Lee <zacharylhlee@gmail.com>
2023-11-28 10:54:21 -0700

    Use DataGrid for PlaylistTable

--- app/components/playlist-table.tsx
+++ app/components/playlist-table.tsx
@@ -1,53 +1,26 @@
 'use client';
-import { styled, TableCell, tableCellClasses, TableRow, TableContainer, Paper, Table, TableHead, TableBody } from "@mui/material";
-import { Playlist } from "../constants/playlist";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { PlaylistColumns } from '../constants/playlist-columns';
+import { Playlist } from '../constants/playlist';
 
 export default function PlaylistTable() {
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 20,
-    },
-  }));
-
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-
   return (
-    <TableContainer className="mt-3" component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="playlist">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Title</StyledTableCell>
-            <StyledTableCell align="right">Artist</StyledTableCell>
-            <StyledTableCell align="right">Album</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Playlist.map((song) => (
-            <StyledTableRow
-              key={song.title}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {song.title}
-              </TableCell>
-              <TableCell align="right">{song.artist}</TableCell>
-              <TableCell align="right">{song.album}</TableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  )
+    <Box sx={{ height: 'max-content', width: '100%', marginTop: '1.25em', }}>
+      <DataGrid
+        sx={{ color: 'white' }}
+        rows={Playlist}
+        columns={PlaylistColumns}
+        initialState={{
+          sorting: {
+            sortModel: [{ field: 'artist', sort: 'asc' }],
+          },
+        }}
+        getRowId={(row) => row.title}
+        hideFooter={true}
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
 }
