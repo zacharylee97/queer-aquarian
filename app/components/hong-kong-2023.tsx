ff4eda24411aab21a24e118079fa331bb019bc0f
Zachary Lee <zacharylhlee@gmail.com>
2023-12-01 10:37:56 -0700

    Added HK 2023 Pages 2 and 3

--- app/components/hong-kong-2023.tsx
+++ app/components/hong-kong-2023.tsx
@@ -10,6 +10,18 @@ export default function HongKong2023() {
         width={500}
         height={500}
       />
+      <Image
+        src="/hong-kong-2023-2.png"
+        alt="Hong Kong 2023 Page 2"
+        width={500}
+        height={500}
+      />
+      <Image
+        src="/hong-kong-2023-3.png"
+        alt="Hong Kong 2023 Page 3"
+        width={500}
+        height={500}
+      />
       <Typography variant="h5">More to come, stay tuned!</Typography>
     </Box>
   );
