61e4abf5964784b59340dd6be69d5c8351018b6b
Zachary Lee <zacharylhlee@gmail.com>
2023-12-09 12:25:08 -0700

    Created Clobs2018

--- app/clobs/page.tsx
+++ app/clobs/page.tsx
@@ -1,24 +1,26 @@
 import { Box, Typography } from "@mui/material";
 import Header from "../components/header";
-import Image from "next/image";
+import Clobs2018 from "../components/clobs-2018";
 
 export default function Clobs() {
   return (
     <main className="min-h-screen w-screen">
       <Header></Header>
       <Box className="flex flex-col mt-28">
-        <Typography className="m-10 mb-3" variant="h3">
-          CLOBS
-        </Typography>
-        <Box className="mx-10">
-          <Image
-            className="mb-5"
-            src="/clobs.png"
-            alt="CLOBS"
-            width={500}
-            height={500}
-          />
-          <Typography variant="h5">More to come, stay tuned!</Typography>
+        <Box className="m-10">
+          <Typography variant="h4">
+            <p>
+              {`Comprehensive List of Bullshit (CLOBS) I've Had to Deal With in
+              Life:`}
+            </p>
+            <p>Growing Up and Living with Anxiety, Depression and OCPD</p>
+          </Typography>
+          <Typography variant="h5">A Memoir By Zachary Lee</Typography>
+          <br />
+          <Typography variant="h5">
+            <p>{`CLOBS I've Had to Deal With (2018 to 2023)`}</p>
+          </Typography>
+          <Clobs2018></Clobs2018>
         </Box>
       </Box>
     </main>
---
+++ app/components/clobs-2018.tsx
@@ -0,0 +1,106 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Clobs2018() {
+  return (
+    <Box>
+      <Typography variant="h5">
+        <p>
+          <b>2018</b>
+        </p>
+      </Typography>
+      <Typography variant="h6">
+        <p>
+          <b>06/14/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`Nobody said this was easy`}</p>
+        <p>{`Nobody said that it would be this hard`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>06/15/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`It's hard for me to feel proud of all the things I have accomplished`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>06/16/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`I don't want to keep hiding who I am`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>06/24/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`It's so difficult to continue when all I can imagine is the worst possible outcome.`}</p>
+        <p>{`How much longer until I make it to the other side?`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>06/26/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`I will never ask for what I want; I will never get what I need`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>06/26/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`I will never ask for what I want; I will never get what I need`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>07/03/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`Do you know what it feels like knowing that none of your dreams will ever come true?`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>07/21/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`I'm learning to be comfortable with being alone`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>07/25/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`It may be surprising, but not everything is as scary as it seems`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h6">
+        <p>
+          <b>08/26/18</b>
+        </p>
+      </Typography>
+      <Typography variant="body1">
+        <p>{`Why can't I just be brave?`}</p>
+      </Typography>
+      <br />
+      <Typography variant="h5">More to come, stay tuned!</Typography>
+    </Box>
+  );
+}
