38bf4e7a85bc774cd3afb8b9f2337faa7770490e
Zachary Lee <zacharylhlee@gmail.com>
2023-12-19 08:24:41 -0700

    Created separate Kpop and Pop constants

---
+++ app/constants/kpop.tsx
@@ -0,0 +1,429 @@
+import { Song } from "../interfaces/song";
+
+export const Kpop: Song[] = [
+  {
+    title: "One More Last Time",
+    artist: "Henry Young feat. Ashley Alisha",
+    album: "Single",
+  },
+  {
+    title: "Drama",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+  },
+  {
+    title: "Trick or Trick",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+  },
+  {
+    title: "YOLO",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+  },
+  {
+    title: "You",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+  },
+  {
+    title: "Thirsty",
+    artist: "aespa",
+    album: "MY WORLD - The 3rd Mini Album",
+  },
+  {
+    title: "I'm Unhappy",
+    artist: "aespa",
+    album: "MY WORLD - The 3rd Mini Album",
+  },
+  {
+    title: "'Til We Meet Again",
+    artist: "aespa",
+    album: "MY WORLD - The 3rd Mini Album",
+  },
+  {
+    title: "Better Things",
+    artist: "aespa",
+    album: "Single",
+  },
+  {
+    title: "Hold On Tight",
+    artist: "aespa",
+    album: "Single",
+  },
+  {
+    title: "Air Force One",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "Je Ne Sais Quoi",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "Lucid",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "My Secret Playlist",
+    artist: "Odd Eye Circle",
+    album: "Version Up",
+  },
+  {
+    title: "MANIAC",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Untie",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Overflow",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Day by day",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Up 2 Me",
+    artist: "VIVIZ",
+    album: "The 4th Mini Album 'VERSUS'",
+  },
+  {
+    title: "Chill Kill",
+    artist: "Red Velvet",
+    album: "Chill Kill - The 3rd Album",
+  },
+  {
+    title: "Will I Ever See You Again",
+    artist: "Red Velvet",
+    album: "Chill Kill - The 3rd Album",
+  },
+  {
+    title: "Psycho",
+    artist: "Red Velvet",
+    album: "'The ReVe Festival' Finale",
+  },
+  {
+    title: "In My Dreams",
+    artist: "Red Velvet",
+    album: "'The ReVe Festival 2022 - Feel My Rhythm'",
+  },
+  {
+    title: "Off The Record",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Baddie",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Either Way",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Holy Moly",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "OTT",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Payback",
+    artist: "IVE",
+    album: "I'VE MINE",
+  },
+  {
+    title: "Sensitive",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Real world",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Colouring",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Newtopia",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Strawberry soda",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "Day by day",
+    artist: "Loossemble",
+    album: "Loossemble",
+  },
+  {
+    title: "GODS",
+    artist: "NewJeans",
+    album: "Single",
+  },
+  {
+    title: "Beautiful Restriction",
+    artist: "NewJeans",
+    album: "Single",
+  },
+  {
+    title: "Our Night is more beautiful than your Day",
+    artist: "NewJeans",
+    album: "Single",
+  },
+  {
+    title: "New Jeans",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "Super Shy",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "ETA",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "Cool With You",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "Get Up",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "ASAP",
+    artist: "NewJeans",
+    album: "NewJeans 2nd EP 'Get Up'",
+  },
+  {
+    title: "OMG",
+    artist: "NewJeans",
+    album: "NewJeans 'OMG'",
+  },
+  {
+    title: "Ditto",
+    artist: "NewJeans",
+    album: "NewJeans 'OMG'",
+  },
+  {
+    title: "Hype Boy",
+    artist: "NewJeans",
+    album: "NewJeans 1st EP 'New Jeans'",
+  },
+  {
+    title: "Hurt",
+    artist: "NewJeans",
+    album: "NewJeans 1st EP 'New Jeans'",
+  },
+  {
+    title: "Yeah Yeah Yeah",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Hard To Love",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Tally",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Ready For Love",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Don't Know What To Do",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Kick It",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Hope Not",
+    artist: "BLACKPINK",
+    album: "Kill This Love",
+  },
+  {
+    title: "Feel Special",
+    artist: "TWICE",
+    album: "Feel Special",
+  },
+  {
+    title: "Talk That Talk",
+    artist: "TWICE",
+    album: "BETWEEN 1&2",
+  },
+  {
+    title: "Basics",
+    artist: "TWICE",
+    album: "BETWEEN 1&2",
+  },
+  {
+    title: "Gone",
+    artist: "TWICE",
+    album: "BETWEEN 1&2",
+  },
+  {
+    title: "DM",
+    artist: "fromis_9",
+    album: "Midnight Guest",
+  },
+  {
+    title: "Attitude",
+    artist: "fromis_9",
+    album: "Unlock My World",
+  },
+  {
+    title: "#menow",
+    artist: "fromis_9",
+    album: "Unlock My World",
+  },
+  {
+    title: "Anywhere But Home",
+    artist: "SEULGI",
+    album: "28 Reasons - The 1st Mini Album",
+  },
+  {
+    title: "Crown",
+    artist: "SEULGI",
+    album: "28 Reasons - The 1st Mini Album",
+  },
+  {
+    title: "When This Rain Stops",
+    artist: "WENDY",
+    album: "Like Water - The 1st Mini Album",
+  },
+  {
+    title: "Like Water",
+    artist: "WENDY",
+    album: "Like Water - The 1st Mini Album",
+  },
+  {
+    title: "The Road",
+    artist: "WENDY",
+    album: "Like Water - The 1st Mini Album",
+  },
+  {
+    title: "Best Friend (with SEULGI)",
+    artist: "WENDY",
+    album: "Like Water - The 1st Mini Album",
+  },
+  {
+    title: "Killin' Me Good",
+    artist: "JIHYO",
+    album: "ZONE",
+  },
+  {
+    title: "Talkin' About It (feat. 24kGoldn)",
+    artist: "JIHYO",
+    album: "ZONE",
+  },
+  {
+    title: "POP!",
+    artist: "NAYEON",
+    album: "IM NAYEON",
+  },
+  {
+    title: "NO PROBLEM (feat. Felix of Stray Kids)",
+    artist: "NAYEON",
+    album: "IM NAYEON",
+  },
+  {
+    title: "LOVE COUNTDOWN (feat. Wonstein)",
+    artist: "NAYEON",
+    album: "IM NAYEON",
+  },
+  {
+    title: "SUNSET",
+    artist: "NAYEON",
+    album: "IM NAYEON",
+  },
+  {
+    title: "INVU",
+    artist: "TAEYEON",
+    album: "INVU - The 3rd Album",
+  },
+  {
+    title: "Dear Me",
+    artist: "TAEYEON",
+    album: "Purpose - The 2nd Album",
+  },
+  {
+    title: "Drawing Our Moments",
+    artist: "TAEYEON",
+    album: "Purpose - The 2nd Album",
+  },
+  {
+    title: "Gravity",
+    artist: "TAEYEON",
+    album: "Purpose - The 2nd Album",
+  },
+  {
+    title: "Blue",
+    artist: "TAEYEON",
+    album: "Four Seasons",
+  },
+  {
+    title: "Howl",
+    artist: "CHUU",
+    album: "Howl",
+  },
+  {
+    title: "Underwater",
+    artist: "CHUU",
+    album: "Howl",
+  },
+  {
+    title: "My Palace",
+    artist: "CHUU",
+    album: "Howl",
+  },
+  {
+    title: "Aliens",
+    artist: "CHUU",
+    album: "Howl",
+  },
+  {
+    title: "Hitchhiker",
+    artist: "CHUU",
+    album: "Howl",
+  },
+  {
+    title: "Video Game",
+    artist: "Heejin",
+    album: "K",
+  },
+];
