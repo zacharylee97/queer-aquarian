670fb98d300dd40ccd0b41eb33017d7c9b63b320
Zachary Lee <zacharylhlee@gmail.com>
2023-12-21 04:48:23 -0700

    Fixed issue with useEffect dependency array

--- app/components/selected-song.tsx
+++ app/components/selected-song.tsx
@@ -1,24 +1,25 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { Playlist } from '../constants/playlist';
-import { Song } from '../interfaces/song';
-import { Typography } from '@mui/material';
+"use client";
+import { useEffect, useState } from "react";
+import { Playlist } from "../constants/playlist";
+import { Song } from "../interfaces/song";
+import { Typography } from "@mui/material";
 
 export default function SelectedSong() {
   const [selectedSong, setSelectedSong] = useState<Song | null>(null);
 
   useEffect(() => {
     setSelectedSong(Playlist[Math.floor(Math.random() * Playlist.length)]);
-  });
+  }, []);
 
   return (
     <Typography className="ml-1" variant="h6">
-      {selectedSong !== null ?
+      {selectedSong !== null ? (
         <i>
           {selectedSong.title} by {selectedSong.artist}
         </i>
-        : <div></div>
-      }
+      ) : (
+        <div></div>
+      )}
     </Typography>
-  )
+  );
 }
