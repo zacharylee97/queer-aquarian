f0e0f450272b3fd367466fb09538cc0d4f4013a9
Zachary Lee <zacharylhlee@gmail.com>
2023-12-21 07:11:00 -0700

    Updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,22 @@
 import Header from "./components/header";
 import Image from "next/image";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import MusicPlayer from "./components/music-player";
 
 export default function Home() {
-  const sloganStyle = {
-    width: "calc(100% - 5rem)",
-    margin: "2.5rem",
-  };
-
   return (
-    <main className="min-h-screen w-screen overflow-hidden">
+    <main className="min-h-screen w-screen">
       <Header></Header>
-      <Box className="flex flex-col relative">
+      <Box className="flex flex-col relative overflow-hidden">
         <MusicPlayer></MusicPlayer>
         <Image
-          src="/background-images/Luna.png"
-          alt="Background Image"
+          src="/home-page.png"
+          alt="Home Page"
           priority
-          fill
           quality={100}
-          sizes="100vw"
-          style={{ objectFit: "cover", zIndex: "-1", opacity: "50%" }}
+          width={5000}
+          height={5000}
         />
-        <Image
-          src="/slogan.png"
-          alt="Slogan"
-          width={1000}
-          height={1000}
-          style={sloganStyle}
-        />
-        <Box className="p-10 bg-black">
-          {`Queer Aquarian is my personal blog where I can post whatever I want.
-          It started out as an art and photography `}
-          <a href="https://www.instagram.com/queer_aquarian">
-            <u>Instagram</u>
-          </a>
-          {` account, but I wanted a place to share my other passions as well, like my love for 
-          music, food and travel. I'm also planning on sharing more personal details of my life.`}
-          <br />
-          <br />
-          {`I hope you enjoy and remember, life is short, so make the most of it!`}
-        </Box>
       </Box>
     </main>
   );
