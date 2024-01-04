61d5d90f0f894a6f842192aebcb53a9a2a489892
Zach Lee <zacharylhlee@gmail.com>
2024-01-04 12:01:00 -0700

    Added filter for playlist

--- app/constants/playlist.tsx
+++ app/constants/playlist.tsx
@@ -2,4 +2,4 @@ import { Song } from "../interfaces/song";
 import { Kpop } from "./kpop";
 import { Pop } from "./pop";
 
-export const Playlist: Song[] = [...Kpop, ...Pop];
+export const Playlist: Song[] = [...Kpop, ...Pop].filter(song => song.display);
