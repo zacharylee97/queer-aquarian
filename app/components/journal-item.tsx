3d833f9e799b510f2de1d766f6b9fcecbf61c815
Zach Lee <zacharylhlee@gmail.com>
2024-01-08 10:58:45 -0700

    Created Journal page

--- app/components/journal-item.tsx
+++ app/components/journal-item.tsx
@@ -1,13 +1,10 @@
 import { Box } from "@mui/material";
 import Image from "next/image";
-import Journal from "./journal-2018";
-import ClobsHK2023 from "./clobs-hk-2023";
+import Journal2018 from "./journal-2018";
 
 export default function ClobsItem({ ...props }) {
   if (props.index === 0) {
-    return <Journal />;
-  } else if (props.index === 1) {
-    return <ClobsHK2023 />;
+    return <Journal2018 />;
   } else {
     return (
       <Box>
