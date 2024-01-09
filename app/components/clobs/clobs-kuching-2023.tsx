092a35ad3fd973fcf735b3a8a495646ff012d96d
Zach Lee <zacharylhlee@gmail.com>
2024-01-09 11:54:28 -0700

    Created CLOBS Kuching 2023

---
+++ app/components/clobs/clobs-kuching-2023.tsx
@@ -0,0 +1,15 @@
+import { Box, List, ListItem, Typography } from "@mui/material";
+
+export default function ClobsKuching2023() {
+  return (
+    <Box
+      sx={{ marginRight: { xs: "0", md: "calc(100% - 50rem)" } }}
+      className="my-5"
+    >
+      <Typography variant="h5">
+        <b>CLOBS I Have to Deal with in Kuching</b>
+        <br />
+      </Typography>
+    </Box>
+  );
+}
