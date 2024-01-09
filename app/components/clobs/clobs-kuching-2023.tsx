055943b988c5cd5c7538838bcbf520c82fa8e3ab
Zach Lee <zacharylhlee@gmail.com>
2024-01-09 12:05:04 -0700

    Added CLOBS Kuching

--- app/components/clobs/clobs-kuching-2023.tsx
+++ app/components/clobs/clobs-kuching-2023.tsx
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function ClobsKuching2023() {
   return (
@@ -8,6 +8,154 @@ export default function ClobsKuching2023() {
     >
       <Typography variant="h5">
         <b>CLOBS I Have to Deal with in Kuching</b>
+        <br />
+      </Typography>
+      <Typography variant="body1">
+        {`Transcribed from texts to my brother, Jeremy`}
+        <br />
+        {`Dec. 6, 2023`}
+        <br />
+        <br />
+        {`In Malaysia, the main issue was that I wanted to rest and recover from the stress of HK`}
+        <br />
+        <br />
+        {`But I wasn't able to, I didn't sleep or eat much`}
+        <br />
+        <br />
+        {`And everyone was stressed too, which made my anxiety worse`}
+        <br />
+        <br />
+        {`I haven't really told you or Megan what happened in Kuching`}
+        <br />
+        <br />
+        {`I plan to type it out eventually, similar to what I did for HK`}
+        <br />
+        <br />
+        {`But it's been hard to think about when I try and write it down`}
+        <br />
+        <br />
+        {`Basically, for 2 days, I was so physically exhausted my legs and arms were paralyzed`}
+        <br />
+        <br />
+        {`Like I needed to be wheelchaired out of the Spring`}
+        <br />
+        <br />
+        {`Nono, it's fine telling you rn`}
+        <br />
+        <br />
+        {`It's moreso when trying to write it out for my CLOBS`}
+        <br />
+        <br />
+        {`Then, that night, I was gonna go to bed, but felt the left side of my face start to freeze and a pain in my chest`}
+        <br />
+        <br />
+        {`So I immediately got up and went outside, telling everyone I wanted to go to the hospital `}
+        <br />
+        <br />
+        {`I asked them to call 911 and get an ambulance, cause I was worried I was having a heart attack or strokes`}
+        <br />
+        <br />
+        {`But for some reason, no one did anything`}
+        <br />
+        <br />
+        {`It took an hour before we were at the emergency , and even then, they didn't let me in until I wad literally yelling at them`}
+        <br />
+        <br />
+        {`They took my vitals and the doctor said I was “fine” and couldn't stay overnight `}
+        <br />
+        <br />
+        {`So we had to go home and make an appt with the psychiatrist the next morning `}
+        <br />
+        <br />
+        {`So I didn't sleep that night, and the next morning, I don't remember why, but Mommy and Aunty Linda started yelling at me`}
+        <br />
+        <br />
+        {`And that caused me to have a panic attack, I was hyperventilating and crying`}
+        <br />
+        <br />
+        {`I had to lie down on the floor and try to calm myself down, cause everyone was yelling at that point`}
+        <br />
+        <br />
+        {`Eventually, I crawled out of the apartment and Daddy helped me down to the lobby `}
+        <br />
+        <br />
+        {`The building manager saw us and asked what was wrong`}
+        <br />
+        <br />
+        {`She called for an ambulance and waited with us until it arrived `}
+        <br />
+        <br />
+        {`Went to emergency for a second time, and got a prescription from the psychiatrist for a benzo`}
+        <br />
+        <br />
+        {`Saw the psychiatrist, where it was just me talking for like an hour`}
+        <br />
+        <br />
+        {`Made an appt with the resident psychiatrist, since this one was only in on Saturdays`}
+        <br />
+        <br />
+        {`Celebrated Popo's bday on Sunday`}
+        <br />
+        <br />
+        {`Then went back for second psychiatric appt on Monday, where he diagnosed me with anxiety, depression and some form of autism`}
+        <br />
+        <br />
+        {`And the rest of the week, I was able to relax and take it slow`}
+        <br />
+        <br />
+        {`Honestly don't remember who was on the phone`}
+        <br />
+        <br />
+        {`I kinda remember you at one point`}
+        <br />
+        <br />
+        {`Megan, Emily and Aunty Mandy too`}
+        <br />
+        <br />
+        {`Well, I only took the benzo two times after, and that was a whole ordeal`}
+        <br />
+        <br />
+        {`Not taking them anymore`}
+        <br />
+        <br />
+        {`Cause psychiatrist said I should take them with antidepressants `}
+        <br />
+        <br />
+        {`Cause benzos don't treat the underlying cause, just alleviate the symptoms, which is why they can become addictive`}
+        <br />
+        <br />
+        {`No, planning to look for a therapist when I'm back in Montreal`}
+        <br />
+        <br />
+        {`Cause I'm planning to go back to Montreal in Feb after CNY`}
+        <br />
+        <br />
+        {`So I don't wanna look for one here, then switch when I'm in MTL`}
+        <br />
+        <br />
+        {`Well, I've been dealing with it on my own my whole life`}
+        <br />
+        <br />
+        {`But now I know there are friends and family who care about me`}
+        <br />
+        <br />
+        {`So that's why I feel comfortable sharing now`}
+        <br />
+        <br />
+        {`Yeah, well honestly I didn't want to go on the HK and Malaysia trip`}
+        <br />
+        <br />
+        {`Cause I knew it would be stressful, esp without you and Megan there`}
+        <br />
+        <br />
+        {`Well, I was hospitalized and had 2 psychiatric assessments`}
+        <br />
+        <br />
+        {`And getting the formal diagnosis has been great, since I already knew I had anxiety and depression`}
+        <br />
+        <br />
+        {`But now I can say I've been diagnosed with it too`}
+        <br />
         <br />
       </Typography>
     </Box>
