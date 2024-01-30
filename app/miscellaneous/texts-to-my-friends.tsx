ba894061ead92bfef1d9571ae9e3e621738913c5
Zach Lee <zacharylhlee@gmail.com>
2024-01-30 12:51:57 -0700

    Added Texts To My Friends

--- app/miscellaneous/texts-to-my-friends.tsx
+++ app/miscellaneous/texts-to-my-friends.tsx
@@ -1,5 +1,313 @@
 import { Box, Typography } from "@mui/material";
 
 export default function TextsToMyFriends() {
-  return <Box className="my-5"></Box>;
+  return (
+    <Box className="my-5">
+      <Typography variant="h5">
+        <b>Transcribed from texts to my friend, Alex</b>
+        <br />
+        <b>Nov. 17, 2023</b>
+        <br />
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {`Hey Alex, how are you?`}
+        <br />
+        <br />
+        {`No worries! Honestly, probably better this way now that I'm heading home lol`}
+        <br />
+        <br />
+        {`Oh no, so much work? Hope you're doing ok`}
+        <br />
+        <br />
+        {`Lol, I actually have anxiety related to medicine`}
+        <br />
+        <br />
+        {`So the first time I went to emergency, I refused to take a random drug from a random doctor in Kuching`}
+        <br />
+        <br />
+        {`Even tho my parents and aunt were pushing me to just take it`}
+        <br />
+        <br />
+        {`Guess what it was? Xanax`}
+        <br />
+        <br />
+        {`And he already said I couldn't stay overnight at the hospital`}
+        <br />
+        <br />
+        {`So I didn't take it and went home`}
+        <br />
+        <br />
+        {`But the next morning, after little sleep and not much to eat, my mom and aunt started yelling at me`}
+        <br />
+        <br />
+        {`For some reason they were mad that I was being “manipulative” and attention-seeking`}
+        <br />
+        <br />
+        {`So I had a panic attack and had to lie on the floor to calm down`}
+        <br />
+        <br />
+        {`Then, as everyone is screaming, I had to use my legs to try and get out of the condo`}
+        <br />
+        <br />
+        {`In the hallway, my dad helped me get down from the 7th floor to the lobby`}
+        <br />
+        <br />
+        {`Where the building manager helped calm me down while waiting for the ambulance`}
+        <br />
+        <br />
+        {`So I went to emergency a second time, and this time the psychiatrist prescribed a benzodiazepine, which is supposed to help calm anxiety`}
+        <br />
+        <br />
+        {`So I took one dose while waiting to see the psychiatrist`}
+        <br />
+        <br />
+        {`Then after a meeting with the psychiatrist, he gave more to take home`}
+        <br />
+        <br />
+        {`Since I needed to wait a few days before seeing the resident psychiatrist`}
+        <br />
+        <br />
+        {`So the first night, as I was getting ready for bed, I was feeling fine`}
+        <br />
+        <br />
+        {`Like sleepy, but calm`}
+        <br />
+        <br />
+        {`Until it was time to take the benzo`}
+        <br />
+        <br />
+        {`I started to feel anxious and panicky, so I quickly took it and went to bed`}
+        <br />
+        <br />
+        {`But I still needed to go to the washroom and drink water and stuff`}
+        <br />
+        <br />
+        {`So I tried asking my parents for help, but they didn't understand that the drug plus the exhaustion from the past week made me unable to move`}
+        <br />
+        <br />
+        {`So that first night was super fun lol`}
+        <br />
+        <br />
+        {`The next night, I told my parents I didn't want to take the drug, they said ok`}
+        <br />
+        <br />
+        {`I slept for 3 hours, then woke up`}
+        <br />
+        <br />
+        {`So I told my mom maybe I should take it`}
+        <br />
+        <br />
+        {`She said ok, but once I did, I felt this weight on my chest and I felt like I couldn't breathe`}
+        <br />
+        <br />
+        {`So I didn't sleep well that night lol`}
+        <br />
+        <br />
+        {`The next day, I went to see the resident psychiatrist, and after a 3 hr long assessment, he diagnoses me with anxiety, depression and some form of autism/OCPD`}
+        <br />
+        <br />
+        {`He also prescribed more benzos, which I haven't taken, given my past reaction to them`}
+        <br />
+        <br />
+        {`So yeah, that's my stressful week in Kuching lol`}
+        <br />
+        <br />
+      </Typography>
+      <Typography variant="h5">
+        <b>Transcribed from texts to my friend, Ting</b>
+        <br />
+        <b>Nov. 22, 2023</b>
+        <br />
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {`Hey Ting, how are you?`}
+        <br />
+        <br />
+        {`I'm good, just arguing with both my mom and dad lol`}
+        <br />
+        <br />
+        {`No, back in Calgary`}
+        <br />
+        <br />
+        {`Dad had 3 cups of coffee and drove 3 hours last night`}
+        <br />
+        <br />
+        {`Had to get back home for Maxie lol`}
+        <br />
+        <br />
+      </Typography>
+      <Typography variant="h5">
+        <b>Nov. 24, 2023</b>
+        <br />
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {`Sigh, with my mom it was like my mental health stuff and with my dad it was my work stuff`}
+        <br />
+        <br />
+        {`And even yesterday and today it hasn't been that great`}
+        <br />
+        <br />
+        {`Like I've been able to rest and take it slow at least`}
+        <br />
+        <br />
+        {`But like for some reason, my mom doesn't understand that I'm not fully recovered yet`}
+        <br />
+        <br />
+        {`Like she's still rushing to get things done and not checking to make sure I'm ok`}
+        <br />
+        <br />
+        {`Like tonight, I wanted dessert after dinner`}
+        <br />
+        <br />
+        {`And for Megan's bday dinner next week, the restaurant has a gift card promotion`}
+        <br />
+        <br />
+        {`It's in Kensington, so I thought it going to Marcus for ice cream would be perfect`}
+        <br />
+        <br />
+        {`But like, it was cold outside, we were still a few blocks from the restaurant and my mom wanted me to go with her`}
+        <br />
+        <br />
+        {`So going to the restaurant was fine, but going back to the car she just rushed off without me`}
+        <br />
+        <br />
+        {`And I had to slowly make my way to the car`}
+        <br />
+        <br />
+        {`Later, she even said she thought to tell my dad to drive up, but thought it didn't matter`}
+        <br />
+        <br />
+        {`Sigh, after that my stomach was hurting and I was shivering so I said I didn't want ice cream anymore`}
+        <br />
+        <br />
+        {`I dunno, maybe I'm overreacting, but it's when these things keep happening`}
+        <br />
+        <br />
+        {`I just don't know what to do`}
+        <br />
+        <br />
+        {`Megan's coming back from Edmonton tomorrow, so maybe things will be better then?`}
+        <br />
+        <br />
+        {`Cause right now it's similar to HK and Kuching where it is just me and my parents`}
+        <br />
+        <br />
+      </Typography>
+      <Typography variant="h5">
+        <b>Transcribed from texts to my friend, Chloe</b>
+        <br />
+        <b>Nov. 23, 2023</b>
+        <br />
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {`Hey Chole, how are you?`}
+        <br />
+        <br />
+        {`I don't know whether to laugh or cry`}
+        <br />
+        <br />
+        {`Hope you're doing ok`}
+        <br />
+        <br />
+        {`Sigh, still just as crazy lol`}
+        <br />
+        <br />
+        {`Like all I want to do is rest`}
+        <br />
+        <br />
+        {`But we drove up to Edmonton Monday afternoon for my sister's convocation`}
+        <br />
+        <br />
+        {`Luckily we left early enough to make it in time for dinner`}
+        <br />
+        <br />
+        {`Then on Tuesday, the morning was fine, but I needed to take a nap before the ceremony`}
+        <br />
+        <br />
+        {`And at 2pm, my sister woke me up abruptly and told me to hurry up cause she needed to be there at 2:30pm`}
+        <br />
+        <br />
+        {`They only told me the ceremony was at 3pm!`}
+        <br />
+        <br />
+        {`So as per usual, they're rushing me to get ready`}
+        <br />
+        <br />
+        {`While I'm just waking up and still exhausted from the past month`}
+        <br />
+        <br />
+        {`The ceremony was fine, but then we had to rush to dinner cause we were late`}
+        <br />
+        <br />
+        {`Then for some reason, even tho we finished dinner at 8pm, we couldn't stay one more night in Edmonton`}
+        <br />
+        <br />
+        {`My dad had to drink 3 cups of coffee and speed all the way home to Calgary`}
+        <br />
+        <br />
+        {`Didn't get back home until 12:30pm, so as you can imagine, I was so exhausted`}
+        <br />
+        <br />
+        {`Even today, I have an appt for eye dilation at 4pm`}
+        <br />
+        <br />
+        {`I was napping and my mom was rushing me to get up and go early`}
+        <br />
+        <br />
+        {`I didn't understand why, and she wasn't even helping me`}
+        <br />
+        <br />
+        {`Anyways, apparently she needed her glasses fixed and has questions for the optometrist`}
+        <br />
+        <br />
+        {`But of course she didn't tell me any of this`}
+        <br />
+        <br />
+        {`I can't wait to go back to Montreal`}
+        <br />
+        <br />
+      </Typography>
+      <Typography variant="h5">
+        <b>Nov. 24, 2023</b>
+        <br />
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        {`Sigh, but I've told them so many times`}
+        <br />
+        <br />
+        {`Like during the HK and Kuching trip`}
+        <br />
+        <br />
+        {`And like before that too`}
+        <br />
+        <br />
+        {`I like to know the plan so I know how much time I have to get ready`}
+        <br />
+        <br />
+        {`Like for Megan's convocation, I thought it was at 3, cause that's what my mom told me`}
+        <br />
+        <br />
+        {`But at 2, my sis woke me up from a nap and was rushing me to get ready`}
+        <br />
+        <br />
+        {`I was like, huh why?`}
+        <br />
+        <br />
+        {`She said she has to be there by 2:30 😒`}
+        <br />
+        <br />
+        {`Things like this, you know? I like to take my time and not rush`}
+        <br />
+        <br />
+        {`Anyways, how are you?`}
+        <br />
+        <br />
+      </Typography>
+    </Box>
+  );
 }
